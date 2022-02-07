ae251e2a43e20dbde69d4ee39f1a0ce0c5d2325f
VKY <you@example.com>
2022-02-07 15:30:39 +0530

    fixed two task

---
+++ basic.java
@@ -0,0 +1,16 @@
+package vky;
+
+public class basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int a=10;
+      int b=20;
+      int x=a-b;
+      int y=a+b;
+      
+      System.getProperty("X , Y");
+      System.out.println("10 & 30");
+	}
+
+}
